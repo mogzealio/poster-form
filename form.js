003b464df144063b39119d4bc9dd520bf14ba7b4
mogzealio <jack@jackphelan.xyz>
2025-10-28 11:27:27 +0000

    Update form.js

--- form.js
+++ form.js
@@ -44,7 +44,7 @@
         
         const CONFIG = {
             workerUrl: 'https://poster-checkout.jack-7a4.workers.dev',
-            successUrl: window.location.origin + '/success', // Change to your success page
+            successUrl: window.location.origin + '/#success', // Change to your success page
             cancelUrl: window.location.origin, // Change to your cancel/home page
             
             // Prices for display (should match your Stripe prices)
